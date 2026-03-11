521aaaefc36d63b4e31a8490a131eaf4639ede45
aptech-208 <aptech.daniyal0@gmail.com>
2026-03-11 14:29:15 +0500

    Tailwind & daisy

---
+++ React-Project/src/Components/Foot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Foot() {
+  return (
+    <div>
+      Its Gonna Be The Footer!
+    </div>
+  )
+}
+
+export default Foot
---
+++ React-Project/src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      Its Gonna Be The Nav Bar!
+    </div>
+  )
+}
+
+export default Nav
